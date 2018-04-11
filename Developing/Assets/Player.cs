c9bc7f865d4b3dbb3920ccb1b40ddd33a0f5fc0e
yti1993 <yti19931011@gmail.com>
2018-04-11 18:52:33 +0900

    2018-04-11
    Developed
    
    1. Card Dragging System
    2. Github communication
    3. Simple system design

---
+++ Developing/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public int HealthPoint = 100;
+
+	private int InitHp;
+	private int EnergyPoint;
+
+
+	// Use this for initialization
+	void Start () {
+		InitHp = HealthPoint;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (HealthPoint < 0)
+			PlayerDie ();
+	}
+
+	void PlayerDie () {
+		//Destroy (GameObject);
+	}
+}
