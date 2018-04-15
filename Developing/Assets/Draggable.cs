f7e27811c95ee98024c2e39fc6b42b410c7d38f9
yti1993 <yti19931011@gmail.com>
2018-04-15 17:50:09 +0900

    Setting Zone
    1. 카드 슈팅존 & 핸딩존 설정
    2. 카드가 사용 되었는지 안되었는지 판별하는 변수 설정

---
+++ Developing/Assets/Draggable.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+
+	//private Vector3 StartingPoint;
+	public Transform parentToReturnTo = null;
+	public enum Slot {Unused, Used};
+	public Slot IsUsed = Slot.Unused;
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		//StartingPoint.x = transform.position.x;
+		//StartingPoint.y = transform.position.y;
+		parentToReturnTo = this.transform.parent;
+		this.transform.SetParent (this.transform.parent.parent);
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+		//Debug.Log ("BeginDragparent : " + this.transform.parent);
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		//Debug.Log ("OnDrag");
+		this.transform.position = eventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		this.transform.SetParent(parentToReturnTo);
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
+		//Debug.Log ("EndDragparent : " + this.transform.parent);
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ("Start X pos : " + StartingPoint.x);
+		//Debug.Log ("Start Y pos : " + StartingPoint.y);
+		//Debug.Log (transform.position.x);
+	}
+
+
+}
---
+++ Developing/Assets/ShootingZone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShootingZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	public Draggable.Slot IsUsed = Draggable.Slot.Used;
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		//Debug.Log ("OnPointerEnter");
+	}
+	public void OnPointerExit(PointerEventData eventData) {
+		//Debug.Log ("OnPointerExit");
+	}
+	public void OnDrop(PointerEventData eventData) {
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable> ();
+		if (d != null) {
+			if (IsUsed != d.IsUsed) {
+				d.parentToReturnTo = this.transform;
+			}
+		}
+	}
+}
